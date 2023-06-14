945dc5023f25a1d6fdd216be92ae6a0f28e2d68b
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2023-06-14 14:49:04 -0400

    styled two buttons html page and large elt html page

--- argue.js
+++ argue.js
@@ -1,9 +1,11 @@
 
 const header = document.getElementById("text");
 document.getElementById("first").addEventListener("click", (e) => {
-    header.textContent = "I'm right!"
+    document.getElementById("first").textContent = "I'm right!";
+    document.getElementById("second").textContent = "";
 });
 
 document.getElementById("second").addEventListener("click", (e) => {
-    header.textContent = "No I'm right!"
+    document.getElementById("second").textContent = "No I'm right!";
+    document.getElementById("first").textContent = "";
 });
